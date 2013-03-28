0a1594fa5bf968e5d5f73292d756b99f132a2fd5
Miao Xie <miaox@cn.fujitsu.com>
2013-03-28 09:03:52 +0000

    Btrfs: remove unnecessary variant in btrfs_sector_sum, structure
    bytenr in btrfs_sector_sum is unnecessary, because the extents that
    btrfs_sector_sum points to are continuous, we can find out the expected
    checksums by btrfs_ordered_sum's bytenr and the offset, so we can
    remove btrfs_sector_sum's bytenr.
    
    After removing bytenr, we don't use the while loop to get the checksums
    one by one. Now, we can get several checksum value at one time. By this
    way, the performance of write is improved by ~74% on my SSD (31MB/s -> 54MB/s)
    
    test command:
     # dd if=/dev/zero of=/mnt/btrfs/file0 bs=1M count=1024 oflag=sync
    
    Signed-off-by: Miao Xie <miaox@cn.fujitsu.com>
    Signed-off-by: Josef Bacik <jbacik@fusionio.com>

--- fs/btrfs/ordered-data.c
+++ fs/btrfs/ordered-data.c
@@ -989,7 +989,6 @@ int btrfs_find_ordered_sum(struct inode *inode, u64 offset, u64 disk_bytenr,
 			   u32 *sum, int len)
 {
 	struct btrfs_ordered_sum *ordered_sum;
-	struct btrfs_sector_sum *sector_sums;
 	struct btrfs_ordered_extent *ordered;
 	struct btrfs_ordered_inode_tree *tree = &BTRFS_I(inode)->ordered_tree;
 	unsigned long num_sectors;
@@ -1006,17 +1005,15 @@ int btrfs_find_ordered_sum(struct inode *inode, u64 offset, u64 disk_bytenr,
 		if (disk_bytenr >= ordered_sum->bytenr &&
 		    disk_bytenr < ordered_sum->bytenr + ordered_sum->len) {
 			i = (disk_bytenr - ordered_sum->bytenr) / sectorsize;
-			sector_sums = ordered_sum->sums + i;
-			num_sectors = ordered_sum->len / sectorsize;
-			for (; i < num_sectors; i++) {
-				if (sector_sums[i].bytenr == disk_bytenr) {
-					sum[index] = sector_sums[i].sum;
-					index++;
-					if (index == len)
-						goto out;
-					disk_bytenr += sectorsize;
-				}
-			}
+			num_sectors = ordered_sum->len / sectorsize - i;
+			num_sectors = min_t(int, len - index, num_sectors);
+			memcpy(sum + index, ordered_sum->sums + i,
+			       num_sectors);
+
+			index += (int)num_sectors;
+			if (index == len)
+				goto out;
+			disk_bytenr += num_sectors * sectorsize;
 		}
 	}
 out:
