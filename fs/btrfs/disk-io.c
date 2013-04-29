1433f07ae634575e28fbaba03d8fa311d7b58033
Josef Bacik <jbacik@fusionio.com>
2013-04-29 10:05:57 -0400

    Btrfs: deal with errors in write_dev_supers
    If you try to mount -o loop a restored file system it will panic if the file
    ends up being smaller than the original disk.  This is because we go to try and
    get a block for a super that may be past the EOF which makes __getblk return
    NULL for a buffer head when we aren't expecting it to.  Fix this by dealing with
    this case and just jacking up the errors count.  With this patch we no longer
    panic when mounting a restored file system loopback.  Thanks,
    
    Signed-off-by: Josef Bacik <jbacik@fusionio.com>

--- fs/btrfs/disk-io.c
+++ fs/btrfs/disk-io.c
@@ -3043,7 +3043,10 @@ static int write_dev_supers(struct btrfs_device *device,
 		if (wait) {
 			bh = __find_get_block(device->bdev, bytenr / 4096,
 					      BTRFS_SUPER_INFO_SIZE);
-			BUG_ON(!bh);
+			if (!bh) {
+				errors++;
+				continue;
+			}
 			wait_on_buffer(bh);
 			if (!buffer_uptodate(bh))
 				errors++;
@@ -3070,6 +3073,13 @@ static int write_dev_supers(struct btrfs_device *device,
 			 */
 			bh = __getblk(device->bdev, bytenr / 4096,
 				      BTRFS_SUPER_INFO_SIZE);
+			if (!bh) {
+				printk(KERN_ERR "btrfs: couldn't get super "
+				       "buffer head for bytenr %Lu\n", bytenr);
+				errors++;
+				continue;
+			}
+
 			memcpy(bh->b_data, sb, BTRFS_SUPER_INFO_SIZE);
 
 			/* one reference for submit_bh */
