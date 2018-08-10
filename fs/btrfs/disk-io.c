6d27565f83f85715ea09cea13acde699e0d7c0a7
Josef Bacik <josef@toxicpanda.com>
2018-08-10 10:42:04 -0400

    btrfs: clear delayed_refs_rsv for dirty bg cleanup
    We keep track of dirty bg's as a reservation in the delayed_refs_rsv, so
    when we abort and we cleanup those dirty bgs we need to drop their
    reservation so we don't have accounting issues and lots of scary
    messages on umount.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- fs/btrfs/disk-io.c
+++ fs/btrfs/disk-io.c
@@ -4412,6 +4412,7 @@ void btrfs_cleanup_dirty_bgs(struct btrfs_transaction *cur_trans,
 
 		spin_unlock(&cur_trans->dirty_bgs_lock);
 		btrfs_put_block_group(cache);
+		btrfs_delayed_refs_rsv_release(fs_info, 1);
 		spin_lock(&cur_trans->dirty_bgs_lock);
 	}
 	spin_unlock(&cur_trans->dirty_bgs_lock);
