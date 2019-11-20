db5475e573359dc58cd901a2941cd695212dd846
Josef Bacik <josef@toxicpanda.com>
2019-11-20 12:36:19 -0800

    btrfs: hold a ref on the root in btrfs_check_uuid_tree_entry
    We lookup the uuid of arbitrary subvolumes, hold a ref on the root while
    we're doing this.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- fs/btrfs/volumes.c
+++ fs/btrfs/volumes.c
@@ -4382,6 +4382,10 @@ static int btrfs_check_uuid_tree_entry(struct btrfs_fs_info *fs_info,
 			ret = 1;
 		goto out;
 	}
+	if (!btrfs_grab_fs_root(subvol_root)) {
+		ret = 1;
+		goto out;
+	}
 
 	switch (type) {
 	case BTRFS_UUID_KEY_SUBVOL:
@@ -4394,7 +4398,7 @@ static int btrfs_check_uuid_tree_entry(struct btrfs_fs_info *fs_info,
 			ret = 1;
 		break;
 	}
-
+	btrfs_put_fs_root(subvol_root);
 out:
 	return ret;
 }
