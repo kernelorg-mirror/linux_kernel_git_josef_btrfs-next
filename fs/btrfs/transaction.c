1da83858d7fd9e5f09dc0fb8571cf71152e1f795
Josef Bacik <josef@toxicpanda.com>
2020-02-27 14:11:32 -0500

    cut some of the debugging

--- fs/btrfs/transaction.c
+++ fs/btrfs/transaction.c
@@ -905,10 +905,8 @@ btrfs_throttle_for_delayed_refs(struct btrfs_fs_info *fs_info,
 		atomic_read(&delayed_refs->entries_run);
 	time64_t start = ktime_get_seconds();
 
-	if (refs > 30) {
-		printk(KERN_ERR "WTF is generating this many delayed refs?\n");
-		dump_stack();
-	}
+	if (refs > 30)
+		printk(KERN_ERR "WTF is generating this many delayed refs? %lu\n", refs);
 
 	spin_lock(&delayed_refs->lock);
 	if (delayed_refs->last_adjustment - start >= 1) {
