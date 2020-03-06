74e27481b54c60b1beeae82542d5438908e3df20
Josef Bacik <josef@toxicpanda.com>
2020-03-06 15:08:40 -0500

    btrfs: do not READA in build_backref_tree
    Here we are just searching down to the bytenr we're building the backref
    tree for, and all of it's paths to the roots.  These bytenrs are not
    guaranteed to be anywhere near each other, so the READA just generates
    extra latency.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- fs/btrfs/relocation.c
+++ fs/btrfs/relocation.c
@@ -759,8 +759,6 @@ struct backref_node *build_backref_tree(struct reloc_control *rc,
 		err = -ENOMEM;
 		goto out;
 	}
-	path1->reada = READA_FORWARD;
-	path2->reada = READA_FORWARD;
 
 	node = alloc_backref_node(cache);
 	if (!node) {
