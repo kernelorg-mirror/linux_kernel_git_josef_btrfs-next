64aed485d55fad764d3b009557858a026a170db7
Josef Bacik <josef@toxicpanda.com>
2020-03-11 11:15:42 -0400

    delete some excessive debugging

--- fs/btrfs/extent-tree.c
+++ fs/btrfs/extent-tree.c
@@ -2213,10 +2213,6 @@ int btrfs_run_delayed_refs(struct btrfs_trans_handle *trans,
 
 	delayed_refs = &trans->transaction->delayed_refs;
 
-	if ((count == 0 || run_all) && atomic_read(&delayed_refs->num_entries))
-		printk(KERN_ERR "doing the delayed ref %s flushing %d\n",
-		       run_all ? "everything" : "pre", atomic_read(&delayed_refs->num_entries));
-
 	if (count == 0)
 		count = delayed_refs->num_heads_ready;
 
