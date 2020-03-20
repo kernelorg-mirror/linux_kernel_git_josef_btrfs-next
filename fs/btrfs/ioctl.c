c9d60af5da53fb583a5aa7bf2244dde1e41ff15b
Josef Bacik <josef@toxicpanda.com>
2020-03-20 10:27:19 -0400

    logical to ino search commit root

--- fs/btrfs/ioctl.c
+++ fs/btrfs/ioctl.c
@@ -4508,6 +4508,7 @@ static long btrfs_ioctl_logical_to_ino(struct btrfs_fs_info *fs_info,
 		ret = -ENOMEM;
 		goto out;
 	}
+	path->search_commit_root = 1;
 
 	inodes = init_data_container(size);
 	if (IS_ERR(inodes)) {
